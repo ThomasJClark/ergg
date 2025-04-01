4991df1217caa4d17d09e8a35914437ef3632f30
Tom Clark <the.thomas.j.clark@gmail.com>
2025-03-31 22:56:56 -0400

    Render the overlay in the DrawStep task group instead of a DX12 hook

--- src/renderer/renderer.cpp
+++ src/renderer/renderer.cpp
@@ -1,6 +1,7 @@
 #include "renderer.hpp"
 
 #include <elden-x/menu/menu_man.hpp>
+#include <elden-x/task.hpp>
 #include <elden-x/window.hpp>
 
 #include <kiero.h>
@@ -24,136 +25,153 @@ ID3D12Device *gg::renderer::impl::device;
 
 static constexpr unsigned int srv_descriptor_count = 1024;
 
-static function<void()> initialize_callback;
-static function<void()> render_callback;
-
 static D3D12_CPU_DESCRIPTOR_HANDLE heap_start_cpu;
 static D3D12_GPU_DESCRIPTOR_HANDLE heap_start_gpu;
 static unsigned int increment_size;
 static vector<int> free_indexes;
 
-static ID3D12DescriptorHeap *render_descriptor_heap{nullptr};
-static ID3D12DescriptorHeap *back_buffers_descriptor_heap{nullptr};
-
-static ID3D12CommandAllocator *command_allocator{nullptr};
-static ID3D12GraphicsCommandList *command_list{nullptr};
 static ID3D12CommandQueue *command_queue{nullptr};
-static ID3D12DescriptorHeap *srv_descriptor_heap{nullptr};
+static IDXGISwapChain3 *swap_chain{nullptr};
 
-struct render_target_st
+struct render_task_st : public er::CS::CSEzTask
 {
-    ID3D12Resource *resource;
-    D3D12_CPU_DESCRIPTOR_HANDLE descriptor_handle;
-};
-static vector<render_target_st> render_targets;
+  private:
+    /**
+     * True if the render has been set up and we can make draw calls each frame
+     */
+    bool initialized{false};
 
-static void setup_render_targets(IDXGISwapChain *swap_chain)
-{
-    for (size_t i = 0; i < render_targets.size(); i++)
+    /**
+     * True if the window was resized on the previous frame. In the FrameBegin step, the render
+     * targets will be re-allocated.
+     */
+    bool resized{false};
+    pair<int, int> prev_window_size;
+
+    /**
+     * Callback to overlay rendering methods passed from dllmain
+     */
+    function<void()> initialize_callback;
+    function<void()> render_callback;
+
+    struct render_target_st
     {
-        swap_chain->GetBuffer(i, IID_PPV_ARGS(&render_targets[i].resource));
-        gg::renderer::impl::device->CreateRenderTargetView(render_targets[i].resource, nullptr,
-                                                           render_targets[i].descriptor_handle);
-    }
-}
-
-static void setup_dx12_resources(IDXGISwapChain *swap_chain)
-{
-    swap_chain->GetDevice(IID_PPV_ARGS(&gg::renderer::impl::device));
-
-    // Allocate a descriptor heap for the frames
-    {
-        DXGI_SWAP_CHAIN_DESC swap_chain_desc;
-        swap_chain->GetDesc(&swap_chain_desc);
-        render_targets.resize(swap_chain_desc.BufferCount);
-
-        auto desc = D3D12_DESCRIPTOR_HEAP_DESC{
-            .Type = D3D12_DESCRIPTOR_HEAP_TYPE_CBV_SRV_UAV,
-            .NumDescriptors = static_cast<unsigned int>(render_targets.size()),
-            .Flags = D3D12_DESCRIPTOR_HEAP_FLAG_SHADER_VISIBLE,
-        };
-        if (gg::renderer::impl::device->CreateDescriptorHeap(
-                &desc, IID_PPV_ARGS(&render_descriptor_heap)) != S_OK)
-        {
-            return;
-        }
-    }
-
-    // Allocate a descriptor heap for shader resource values (textures and other buffers)
-    {
-        auto desc = D3D12_DESCRIPTOR_HEAP_DESC{
-            .Type = D3D12_DESCRIPTOR_HEAP_TYPE_CBV_SRV_UAV,
-            .NumDescriptors = srv_descriptor_count,
-            .Flags = D3D12_DESCRIPTOR_HEAP_FLAG_SHADER_VISIBLE,
-        };
-        if (gg::renderer::impl::device->CreateDescriptorHeap(
-                &desc, IID_PPV_ARGS(&srv_descriptor_heap)) != S_OK)
-        {
-            return;
-        }
-    }
-    {
-        auto desc = srv_descriptor_heap->GetDesc();
-        heap_start_cpu = srv_descriptor_heap->GetCPUDescriptorHandleForHeapStart();
-        heap_start_gpu = srv_descriptor_heap->GetGPUDescriptorHandleForHeapStart();
-        increment_size = gg::renderer::impl::device->GetDescriptorHandleIncrementSize(desc.Type);
-        free_indexes.reserve((int)desc.NumDescriptors);
-        for (int n = desc.NumDescriptors; n > 0; n--)
-        {
-            free_indexes.push_back(n);
-        }
-    }
-
-    // Create the other bullshit you need to use DX12
-    if (gg::renderer::impl::device->CreateCommandAllocator(
-            D3D12_COMMAND_LIST_TYPE_DIRECT, IID_PPV_ARGS(&command_allocator)) != S_OK)
-    {
-        return;
-    }
-
-    if (gg::renderer::impl::device->CreateCommandList(0, D3D12_COMMAND_LIST_TYPE_DIRECT,
-                                                      command_allocator, NULL,
-                                                      IID_PPV_ARGS(&command_list)) != S_OK ||
-        command_list->Close() != S_OK)
-    {
-        return;
-    }
-
-    auto back_buffers_heap_desc = D3D12_DESCRIPTOR_HEAP_DESC{
-        .Type = D3D12_DESCRIPTOR_HEAP_TYPE_RTV,
-        .NumDescriptors = static_cast<unsigned int>(render_targets.size()),
-        .Flags = D3D12_DESCRIPTOR_HEAP_FLAG_NONE,
-        .NodeMask = 1,
+        ID3D12Resource *resource;
+        D3D12_CPU_DESCRIPTOR_HANDLE descriptor_handle;
     };
-    if (gg::renderer::impl::device->CreateDescriptorHeap(
-            &back_buffers_heap_desc, IID_PPV_ARGS(&back_buffers_descriptor_heap)) != S_OK)
+
+    vector<render_target_st> render_targets;
+
+    ID3D12DescriptorHeap *render_descriptor_heap{nullptr};
+    ID3D12DescriptorHeap *back_buffers_descriptor_heap{nullptr};
+
+    ID3D12CommandAllocator *command_allocator{nullptr};
+    ID3D12GraphicsCommandList *command_list{nullptr};
+    ID3D12DescriptorHeap *srv_descriptor_heap{nullptr};
+
+    void release_render_targets()
     {
-        return;
+        for (auto &frame : render_targets)
+        {
+            frame.resource->Release();
+        }
     }
 
-    auto descriptor_handle = back_buffers_descriptor_heap->GetCPUDescriptorHandleForHeapStart();
-    const auto increment_size = gg::renderer::impl::device->GetDescriptorHandleIncrementSize(
-        D3D12_DESCRIPTOR_HEAP_TYPE_RTV);
-
-    for (size_t i = 0; i < render_targets.size(); i++)
+    void setup_render_targets()
     {
-        render_targets[i].descriptor_handle.ptr = descriptor_handle.ptr + i * increment_size;
+        for (size_t i = 0; i < render_targets.size(); i++)
+        {
+            swap_chain->GetBuffer(i, IID_PPV_ARGS(&render_targets[i].resource));
+            gg::renderer::impl::device->CreateRenderTargetView(render_targets[i].resource, nullptr,
+                                                               render_targets[i].descriptor_handle);
+        }
     }
 
-    setup_render_targets(swap_chain);
-}
-
-static void render_imgui_overlay(IDXGISwapChain3 *swap_chain)
-{
-    using namespace gg::renderer::impl;
-
-    // Set up ImGui the first time this is called
-    static bool imgui_initialized = false;
-    if (!imgui_initialized)
+    void initialize()
     {
-        swap_chain->GetDevice(IID_PPV_ARGS(&device));
+        swap_chain->GetDevice(IID_PPV_ARGS(&gg::renderer::impl::device));
 
-        setup_dx12_resources(swap_chain);
+        // Allocate a descriptor heap for the frames
+        {
+            DXGI_SWAP_CHAIN_DESC swap_chain_desc;
+            swap_chain->GetDesc(&swap_chain_desc);
+            render_targets.resize(swap_chain_desc.BufferCount);
+
+            auto desc = D3D12_DESCRIPTOR_HEAP_DESC{
+                .Type = D3D12_DESCRIPTOR_HEAP_TYPE_CBV_SRV_UAV,
+                .NumDescriptors = static_cast<unsigned int>(render_targets.size()),
+                .Flags = D3D12_DESCRIPTOR_HEAP_FLAG_SHADER_VISIBLE,
+            };
+            if (gg::renderer::impl::device->CreateDescriptorHeap(
+                    &desc, IID_PPV_ARGS(&render_descriptor_heap)) != S_OK)
+            {
+                return;
+            }
+        }
+
+        // Allocate a descriptor heap for shader resource values (textures and other buffers)
+        {
+            auto desc = D3D12_DESCRIPTOR_HEAP_DESC{
+                .Type = D3D12_DESCRIPTOR_HEAP_TYPE_CBV_SRV_UAV,
+                .NumDescriptors = srv_descriptor_count,
+                .Flags = D3D12_DESCRIPTOR_HEAP_FLAG_SHADER_VISIBLE,
+            };
+            if (gg::renderer::impl::device->CreateDescriptorHeap(
+                    &desc, IID_PPV_ARGS(&srv_descriptor_heap)) != S_OK)
+            {
+                return;
+            }
+        }
+        {
+            auto desc = srv_descriptor_heap->GetDesc();
+            heap_start_cpu = srv_descriptor_heap->GetCPUDescriptorHandleForHeapStart();
+            heap_start_gpu = srv_descriptor_heap->GetGPUDescriptorHandleForHeapStart();
+            increment_size =
+                gg::renderer::impl::device->GetDescriptorHandleIncrementSize(desc.Type);
+            free_indexes.reserve((int)desc.NumDescriptors);
+            for (int n = desc.NumDescriptors; n > 0; n--)
+            {
+                free_indexes.push_back(n);
+            }
+        }
+
+        // Create the other bullshit you need to use DX12
+        if (gg::renderer::impl::device->CreateCommandAllocator(
+                D3D12_COMMAND_LIST_TYPE_DIRECT, IID_PPV_ARGS(&command_allocator)) != S_OK)
+        {
+            return;
+        }
+
+        if (gg::renderer::impl::device->CreateCommandList(0, D3D12_COMMAND_LIST_TYPE_DIRECT,
+                                                          command_allocator, NULL,
+                                                          IID_PPV_ARGS(&command_list)) != S_OK ||
+            command_list->Close() != S_OK)
+        {
+            return;
+        }
+
+        auto back_buffers_heap_desc = D3D12_DESCRIPTOR_HEAP_DESC{
+            .Type = D3D12_DESCRIPTOR_HEAP_TYPE_RTV,
+            .NumDescriptors = static_cast<unsigned int>(render_targets.size()),
+            .Flags = D3D12_DESCRIPTOR_HEAP_FLAG_NONE,
+            .NodeMask = 1,
+        };
+        if (gg::renderer::impl::device->CreateDescriptorHeap(
+                &back_buffers_heap_desc, IID_PPV_ARGS(&back_buffers_descriptor_heap)) != S_OK)
+        {
+            return;
+        }
+
+        auto descriptor_handle = back_buffers_descriptor_heap->GetCPUDescriptorHandleForHeapStart();
+        const auto increment_size = gg::renderer::impl::device->GetDescriptorHandleIncrementSize(
+            D3D12_DESCRIPTOR_HEAP_TYPE_RTV);
+
+        for (size_t i = 0; i < render_targets.size(); i++)
+        {
+            render_targets[i].descriptor_handle.ptr = descriptor_handle.ptr + i * increment_size;
+        }
+
+        setup_render_targets();
 
         ImGui::CreateContext();
         ImGui::GetIO().ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;
@@ -161,15 +179,15 @@ static void render_imgui_overlay(IDXGISwapChain3 *swap_chain)
         initialize_callback();
 
         ImGui_ImplDX12_InitInfo init_info;
-        init_info.Device = device;
+        init_info.Device = gg::renderer::impl::device;
         init_info.NumFramesInFlight = render_targets.size();
         init_info.RTVFormat = DXGI_FORMAT_R10G10B10A2_UNORM;
         init_info.SrvDescriptorHeap = render_descriptor_heap;
         init_info.SrvDescriptorAllocFn = [](auto, auto *cpu_handle, auto *gpu_handle) {
-            tie(*cpu_handle, *gpu_handle) = alloc_descriptor();
+            tie(*cpu_handle, *gpu_handle) = gg::renderer::impl::alloc_descriptor();
         };
         init_info.SrvDescriptorFreeFn = [](auto, auto cpu_handle, auto gpu_handle) {
-            free_descriptor({cpu_handle, gpu_handle});
+            gg::renderer::impl::free_descriptor({cpu_handle, gpu_handle});
         };
 
         auto cs_window = er::CS::CSWindow::instance();
@@ -180,45 +198,103 @@ static void render_imgui_overlay(IDXGISwapChain3 *swap_chain)
 
         ImGui::GetMainViewport()->PlatformHandleRaw = cs_window->hwnd;
 
-        imgui_initialized = true;
+        initialized = true;
     }
 
-    ImGui_ImplDX12_NewFrame();
-    ImGui_ImplWin32_NewFrame();
+    void frame_begin()
+    {
+        if (resized)
+        {
+            resized = false;
+            setup_render_targets();
+        }
+    }
 
-    ImGui::NewFrame();
-    render_callback();
-    ImGui::EndFrame();
-    ImGui::Render();
+    void draw_step()
+    {
+        if (!command_queue || !swap_chain)
+        {
+            return;
+        }
 
-    auto &render_target = render_targets[swap_chain->GetCurrentBackBufferIndex()];
+        // Set up ImGui the first time this is called
+        if (!initialized)
+        {
+            initialize();
+        }
 
-    command_allocator->Reset();
+        ImGui_ImplDX12_NewFrame();
+        ImGui_ImplWin32_NewFrame();
 
-    auto resource_barrier = D3D12_RESOURCE_BARRIER{
-        .Type = D3D12_RESOURCE_BARRIER_TYPE_TRANSITION,
-        .Flags = D3D12_RESOURCE_BARRIER_FLAG_NONE,
-        .Transition = {.pResource = render_target.resource,
-                       .Subresource = D3D12_RESOURCE_BARRIER_ALL_SUBRESOURCES,
-                       .StateBefore = D3D12_RESOURCE_STATE_PRESENT,
-                       .StateAfter = D3D12_RESOURCE_STATE_RENDER_TARGET}};
+        ImGui::NewFrame();
+        render_callback();
+        ImGui::EndFrame();
+        ImGui::Render();
 
-    command_list->Reset(command_allocator, nullptr);
-    command_list->ResourceBarrier(1, &resource_barrier);
-    command_list->OMSetRenderTargets(1, &render_target.descriptor_handle, FALSE, nullptr);
-    command_list->SetDescriptorHeaps(1, &render_descriptor_heap);
+        auto &render_target = render_targets[swap_chain->GetCurrentBackBufferIndex()];
 
-    ImGui_ImplDX12_RenderDrawData(ImGui::GetDrawData(), command_list);
+        command_allocator->Reset();
 
-    resource_barrier.Transition.StateBefore = D3D12_RESOURCE_STATE_RENDER_TARGET;
-    resource_barrier.Transition.StateAfter = D3D12_RESOURCE_STATE_PRESENT;
+        auto resource_barrier = D3D12_RESOURCE_BARRIER{
+            .Type = D3D12_RESOURCE_BARRIER_TYPE_TRANSITION,
+            .Flags = D3D12_RESOURCE_BARRIER_FLAG_NONE,
+            .Transition = {.pResource = render_target.resource,
+                           .Subresource = D3D12_RESOURCE_BARRIER_ALL_SUBRESOURCES,
+                           .StateBefore = D3D12_RESOURCE_STATE_PRESENT,
+                           .StateAfter = D3D12_RESOURCE_STATE_RENDER_TARGET}};
 
-    command_list->ResourceBarrier(1, &resource_barrier);
-    command_list->Close();
+        command_list->Reset(command_allocator, nullptr);
+        command_list->ResourceBarrier(1, &resource_barrier);
+        command_list->OMSetRenderTargets(1, &render_target.descriptor_handle, FALSE, nullptr);
+        command_list->SetDescriptorHeaps(1, &render_descriptor_heap);
 
-    command_queue->ExecuteCommandLists(1,
-                                       reinterpret_cast<ID3D12CommandList *const *>(&command_list));
-}
+        ImGui_ImplDX12_RenderDrawData(ImGui::GetDrawData(), command_list);
+
+        resource_barrier.Transition.StateBefore = D3D12_RESOURCE_STATE_RENDER_TARGET;
+        resource_barrier.Transition.StateAfter = D3D12_RESOURCE_STATE_PRESENT;
+
+        command_list->ResourceBarrier(1, &resource_barrier);
+        command_list->Close();
+
+        command_queue->ExecuteCommandLists(
+            1, reinterpret_cast<ID3D12CommandList *const *>(&command_list));
+    }
+
+    void frame_end()
+    {
+        auto window = er::CS::CSWindow::instance();
+        if (window && initialized && window->window_size != prev_window_size)
+        {
+            resized = true;
+            prev_window_size = window->window_size;
+            release_render_targets();
+        }
+    }
+
+  public:
+    render_task_st(function<void()> initialize_callback, function<void()> render_callback)
+        : initialize_callback(initialize_callback), render_callback(render_callback)
+    {
+    }
+
+    virtual void execute(er::FD4::task_data *data) override
+    {
+        switch (data->group)
+        {
+        case er::task_group::FrameBegin:
+            frame_begin();
+            break;
+
+        case er::task_group::DrawStep:
+            draw_step();
+            break;
+
+        case er::task_group::FrameEnd:
+            frame_end();
+            break;
+        }
+    }
+};
 
 /**
  * WNDPROC callback
@@ -241,7 +317,7 @@ static LRESULT wndproc_hook(HWND hwnd, unsigned int msg, WPARAM wparam, LPARAM l
 /**
  * Hook for IDXGISwapChain::Present()
  *
- * Calls ImGui to add custom stuff to the command queue, then calls the original implementation
+ * Stores a pointer to the swap chain so we can render a custom overlay
  *
  * https://learn.microsoft.com/en-us/windows/win32/api/dxgi/nf-dxgi-idxgiswapchain-present
  */
@@ -251,35 +327,10 @@ static HRESULT APIENTRY swap_chain_present_hook(IDXGISwapChain3 *_this, unsigned
 {
     if (command_queue)
     {
-        render_imgui_overlay(_this);
+        swap_chain = _this;
     }
 
     return swap_chain_present(_this, sync_interval, flags);
-}
-
-/**
- * Hook for IDXGISwapChain::ResizeBuffers()
- *
- * Reallocates the render targets after resizing the window
- *
- * https://learn.microsoft.com/en-us/windows/win32/api/d3d12/nf-d3d12-id3d12commandqueue-executecommandlists
- */
-static HRESULT(APIENTRY *swap_chain_resize_buffers)(IDXGISwapChain3 *, unsigned int, unsigned int,
-                                                    unsigned int, DXGI_FORMAT, unsigned int);
-static HRESULT swap_chain_resize_buffers_hook(IDXGISwapChain3 *_this, unsigned int buffer_count,
-                                              unsigned int width, unsigned int height,
-                                              DXGI_FORMAT new_format, unsigned int flags)
-{
-    for (auto &frame : render_targets)
-    {
-        frame.resource->Release();
-    }
-
-    auto hr = swap_chain_resize_buffers(_this, buffer_count, width, height, new_format, flags);
-
-    setup_render_targets(_this);
-
-    return hr;
 }
 
 /**
@@ -323,16 +374,20 @@ void gg::renderer::impl::free_descriptor(gg::renderer::impl::descriptor_pair pai
 void gg::renderer::initialize(function<void()> initialize_callback,
                               function<void()> render_callback)
 {
-    ::initialize_callback = initialize_callback;
-    ::render_callback = render_callback;
-
     auto hwnd = er::CS::CSWindow::instance()->hwnd;
-    wndproc = (WNDPROC)SetWindowLongPtrW(hwnd, GWLP_WNDPROC, (__int3264)(LONG_PTR)wndproc_hook);
+    wndproc = (WNDPROC)SetWindowLongPtrW(hwnd, GWLP_WNDPROC, (LONG_PTR)wndproc_hook);
 
     // https://github.com/Rebzzel/kiero/blob/1.2.12/METHODSTABLE.txt
     kiero::init(kiero::RenderType::D3D12);
     kiero::bind(54, (void **)&command_queue_execute_command_lists,
                 command_queue_execute_command_lists_hook);
     kiero::bind(140, (void **)&swap_chain_present, swap_chain_present_hook);
-    kiero::bind(145, (void **)&swap_chain_resize_buffers, swap_chain_resize_buffers_hook);
+
+    // Register a task to draw the overlay. This could also be done in the DX12 hook, but that
+    // tends not to play nicely with capture software and other overlays.
+    static auto render_task = render_task_st{initialize_callback, render_callback};
+    auto task_man = er::CS::CSTask::instance();
+    task_man->register_task(er::task_group::FrameBegin, render_task);
+    task_man->register_task(er::task_group::DrawStep, render_task);
+    task_man->register_task(er::task_group::FrameEnd, render_task);
 }
